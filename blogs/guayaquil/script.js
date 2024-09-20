c249f4fd7fc12a4c7d555a149d13650ba89bab83
itzHenry1 <trscrus@gmail.com>
2024-09-19 21:12:10 -0500

    test

--- blogs/guayaquil/script.js
+++ blogs/guayaquil/script.js
@@ -95,3 +95,27 @@ boton.addEventListener('click', function() {
        
     }
 })
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to add 'visible' class to the element when it's in view
+function handleScroll() {
+  const elements = document.querySelectorAll('.scrollear');
+
+  elements.forEach(el => {
+      if (isElementInViewport(el)) {
+          el.classList.add('visible');
+      }
+  });
+}
+
+// Listen for the scroll event
+window.addEventListener('scroll', handleScroll);
