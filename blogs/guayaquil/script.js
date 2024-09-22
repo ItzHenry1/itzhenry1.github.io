5a9a78c66741c112036acb711837725c35a6e6c6
itzHenry1 <trscrus@gmail.com>
2024-09-22 14:21:03 -0500

    test

--- blogs/guayaquil/script.js
+++ blogs/guayaquil/script.js
@@ -1,83 +1,5 @@
 const boton = document.getElementById('alternar')
 
-particlesJS("particula1", {
-    particles: {
-      number: { value: 100 }, // Adjust the number of particles
-      color: { value: "#ffffff" }, // Particle color
-      shape: {
-        type: "line", // This can be "line" for stick-like particles
-      },
-      opacity: {
-        value: -1,
-        random: true
-      },
-      size: {
-        value: 1,
-      },
-      line_linked: {
-        enable: true, // Connecting lines between particles
-        distance: 100,
-        color: "#ffffff",
-        opacity: 0.5,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 3, // Adjust movement speed
-      },
-    },
-    interactivity: {
-      events: {
-        onhover: {
-          enable: true,
-          mode: "repulse", // Effect when you hover your cursor
-        },
-      },
-    },
-  });
-
-  particlesJS("particula2", {
-    particles: {
-      number: { value: 100 }, // Adjust the number of particles
-      color: { value: "#ffffff" }, // Particle color
-      shape: {
-        type: "line", // This can be "line" for stick-like particles
-      },
-      opacity: {
-        value: 1,
-        random: true
-      },
-      size: {
-        value: 0,
-      },
-      line_linked: {
-        enable: true, // Connecting lines between particles
-        distance: 85,
-        color: "#ffffff",
-        opacity: 0.5,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 2, // Adjust movement speed
-      },
-    },
-    interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: false,
-        mode: "repulse"
-      },
-      onclick: {
-        enable: true,
-        mode: "push"
-      },
-      resize: true
-    }
-},
-  });
-
 boton.addEventListener('click', function() {
     const cuerpo = document.getElementById('cuerpo')
 
@@ -119,3 +41,39 @@ function handleScroll() {
 
 // Listen for the scroll event
 window.addEventListener('scroll', handleScroll);
+
+particlesJS("particula1", {
+  particles: {
+    number: { value: 90 }, // Adjust the number of particles
+    color: { value: "#ffffff" }, // Particle color
+    shape: {
+      type: "line", // This can be "line" for stick-like particles
+    },
+    opacity: {
+      value: -1,
+      random: true
+    },
+    size: {
+      value: 1,
+    },
+    line_linked: {
+      enable: true, // Connecting lines between particles
+      distance: 90,
+      color: "#ffffff",
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 3, // Adjust movement speed
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse", // Effect when you hover your cursor
+      },
+    },
+  },
+});
